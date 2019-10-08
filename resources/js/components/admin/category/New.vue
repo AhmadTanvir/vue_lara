719bfe199f7b8e96e9d98e3967df411237ca7d23
AhmadTanvir <atasiddiq@gmail.com>
2019-10-09 05:06:48 +0600

    Update New.vue

--- resources/js/components/admin/category/New.vue
+++ resources/js/components/admin/category/New.vue
@@ -2,7 +2,6 @@
     <section class="content mt-3">
         <div class="container-fluid">
             <div class="row justify-content-around">
-                <div></div>
                 <div class="col-md-6">
                     <div class="card card-primary">
                         <div class="card-header text-center">
@@ -10,11 +9,12 @@
                         </div>
                         <!-- /.card-header -->
                         <!-- form start -->
-                        <form role="form">
+                        <form role="form" @click.prevent="addCategory()">
                             <div class="card-body">
                                 <div class="form-group">
                                     <label for="categoryid">Add New Category</label>
-                                    <input type="text" class="form-control" id="categoryid" placeholder="Add New Category">
+                                    <input type="text" v-model="form.catname" name="catname" class="form-control" id="categoryid" placeholder="Add New Category" :class="{ 'is-invalid': form.errors.has('catname') }">
+                                    <has-error :form="form" field="catname"></has-error>
                                 </div>
                             </div>
                             <!-- /.card-body -->
@@ -39,7 +39,21 @@ export default {
         }
     },
     methods: {
-
+        addCategory() {
+            this.$Progress.start();
+            this.form.post('/add-category')
+                .then((response) => {
+                    this.$router.push('/category-list')
+                    toast.fire({
+                        type: 'success',
+                        title: 'Category Added successfully !!'
+                    });
+                    this.$Progress.finish();
+                }).catch(() => {
+                    this.$Progress.fail();
+                })
+            // console.log("Category")
+        }
     }
 }
 
