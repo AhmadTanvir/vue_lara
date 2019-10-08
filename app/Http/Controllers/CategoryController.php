f75778123c65a3d44b1a18696f2e0199cf89985e
AhmadTanvir <atasiddiq@gmail.com>
2019-10-09 05:05:34 +0600

    Update CategoryController.php

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,9 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    //
+    public function add_category(Request $request)
+    {
+    	$this->validate($request, [
+            'catname' => 'required|string|min:3|max:191'
+        ]);
+
+    	$category = new Category();
+    	$category->catname = $request->catname;
+    	$category->save();
+    	return ['message' => 'Data Created Successfully !!'];
+    }
+    public function all_category()
+    {
+    	$categories = Category::all();
+    	return response()->json([
+    		'categories' => $categories
+    	], 200);
+    }
 }
