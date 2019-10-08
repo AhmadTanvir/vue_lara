fc2ccef4738c78dd842565f89ec900bb6556e880
AhmadTanvir <atasiddiq@gmail.com>
2019-10-08 23:48:16 +0600

    initial commit

---
+++ resources/js/components/admin/category/New.vue
@@ -0,0 +1,38 @@
+<template>
+    <section class="content mt-3">
+        <div class="container-fluid">
+            <div class="row justify-content-around">
+                <div></div>
+                <div class="col-md-6">
+                    <div class="card card-primary">
+                        <div class="card-header text-center">
+                            <h3 class="card-title">Add New Category</h3>
+                        </div>
+                        <!-- /.card-header -->
+                        <!-- form start -->
+                        <form role="form">
+                            <div class="card-body">
+                                <div class="form-group">
+                                    <label for="categoryid">Add New Category</label>
+                                    <input type="text" class="form-control" id="categoryid" placeholder="Add New Category">
+                                </div>
+                            </div>
+                            <!-- /.card-body -->
+                            <div class="card-footer">
+                                <button type="submit" class="btn btn-primary float-right">Save</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+<script>
+export default {
+    name: "New"
+}
+
+</script>
+<style scoped>
+</style>
